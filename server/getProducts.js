33471c0ec18abce699f0632f48672f0940ef6ad5
NartayKasim <77471664+NartayKasim@users.noreply.github.com>
2021-11-03 17:13:25 -0500

    fix

--- server/getProducts.js
+++ server/getProducts.js
@@ -2,14 +2,12 @@ const products = require('../products.json')
 
 const getProducts = (req, res) => {
     if (req.query.price) {
-        const items = products.filter(val => val.price >= parseInt(req.query.price))
-
-        items.length > 0 
-        ? 
-        res.status(200).send(items) 
-        : 
-        res.status(200).send(products)
+        const items = products.filter(
+            (val) => val.price >= parseInt(req.query.price)
+        )
+        return res.status(200).send(items)
     }
+    res.status(200).send(products)
 }
 
 module.exports = getProducts
